2b09d7a4ce7dfb7182dbffc6751d998333d5e280
BernardSaturn <beeh.geek@gmail.com>
2024-06-10 14:33:50 -0300

    Implementação completa do CRUD de Produto com relacionamento com Categoria

--- src/main/java/com/generation/drogariagen/model/Categoria.java
+++ src/main/java/com/generation/drogariagen/model/Categoria.java
@@ -1,9 +1,16 @@
 package com.generation.drogariagen.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -23,6 +30,10 @@ public class Categoria {
 	@Size(max = 100, message = " O atributo NOME deve ter no maximo 100 caracteres.")
 	private String descricao;
 
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produtos> produtos;
+	
 	public Long getId() {
 		return id;
 	}
@@ -45,6 +56,14 @@ public class Categoria {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+
+	public List<Produtos> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produtos> produtos) {
+		this.produtos = produtos;
 	}
 	
 	
