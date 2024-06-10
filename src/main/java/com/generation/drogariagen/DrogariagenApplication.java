99c68bddb9bc9602bf7be1a3cb0eea68f5f5045e
BernardSaturn <beeh.geek@gmail.com>
2024-06-10 11:22:10 -0300

    Configurando o projeto inicial

---
+++ src/main/java/com/generation/drogariagen/DrogariagenApplication.java
@@ -0,0 +1,13 @@
+package com.generation.drogariagen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrogariagenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrogariagenApplication.class, args);
+	}
+
+}
